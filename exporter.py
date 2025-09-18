cec682c734a717a3adfcd06d5b5eca3ebedd5e2d
ing-bwrxrp <ing-bwrxrp@gpu001.cm.cluster>
2025-09-18 23:06:30 +0200

    ok

--- exporter.py
+++ exporter.py
@@ -2,6 +2,6 @@ from ultralytics import RTDETR
 import os
 
 model = RTDETR("rtdetr-l.pt")
-model.export(format="engine", dynamic=True)  # export to TensorRT engine with dynamic shape
+model.export(format="engine")  # export to TensorRT engine with dynamic shape
 os.makedirs("model_repository/rtdetr_tensorrt/1", exist_ok=True)
 os.rename("rtdetr-l.engine", "model_repository/rtdetr_tensorrt/1/model.plan")
